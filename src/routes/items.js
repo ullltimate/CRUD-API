c285f878a988c9f42a96bca9ecb65c047ef25131
ullltimate <ullltimate.zp@gmail.com>
2023-07-12 16:57:51 +0300

    refactor: remove comments

--- src/routes/items.js
+++ src/routes/items.js
@@ -1,9 +1,6 @@
-// import required essentials
 const express = require("express");
-// create new router
 const router = express.Router();
 
-// create a JSON data array
 let data = [
   {
     id: 1,
@@ -37,20 +34,15 @@ let data = [
   },
 ];
 
-// this end-point of an API returns JSON data array
 router.get("/", function (req, res) {
   res.status(200).json(data);
 });
 
-// this end-point returns an object from a data array find by id
-// we get `id` from URL end-points
 router.get("/:id", function (req, res) {
-  // find an object from `data` array match by `id`
   let found = data.find(function (item) {
     return item.id === parseInt(req.params.id);
   });
   console.debug(found);
-  // if object found return an object else return 404 not-found
   if (checkValidId(req.params.id)){
     if (found) {
         res.status(200).json(found);
@@ -74,12 +66,10 @@ router.post('/', function(req, res){
 })
 
 router.delete('/:id', (req, res) => {
-    // Reading isbn from the URL
     const id = Number(req.params.id);
     let found = data.find(function (item) {
         return item.id === parseInt(req.params.id);
       });
-    // Remove item from the books array
     if (checkValidId(req.params.id)){
         if(found){
             data = data.filter(i => i.id !== id);
